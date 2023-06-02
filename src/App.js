811e6e980e29dca2d1793b714e40197c4b24b55e
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-06-02 13:51:48 +0530

    question generation completed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import "./App.css"
-import axios from 'axios';
-import Card from './components/Card';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Card from "./components/Card";
 
 export default function App() {
   const [data, setData] = useState(null);
   const [question, setQuestion] = useState(null);
   const [answer, setAnswer] = useState(null);
-  const [options, setOptions] = useState([])
+  const [options, setOptions] = useState([]);
+  const [optionClicked, setOptionClicked] = useState(false);
 
   useEffect(() => {
-    axios("https://restcountries.com/v3.1/all").then((res) => { setData(res.data); getQuestion(res.data) })
-  }, [])
-  console.log(data);
+    axios("https://restcountries.com/v3.1/all").then((res) => {
+      setData(res.data);
+      getQuestion(res.data);
+    });
+  }, []);
   const getQuestion = (data) => {
     var random = Math.floor(Math.random() * 250);
-    setQuestion(data && data[random].capital[0])
+    setQuestion(data && data[random].capital[0]);
     setAnswer(data && data[random].name.common);
     var options = [];
     for (let i = 0; i < 4; i++) {
-      options.push(data[random + i].name.common)
+      options.push(data[random + i].name.common);
     }
-    setOptions(options)
-  }
+    setOptions(options);
+  };
+  const checkAnswer = (ans) => {
+    setOptionClicked(true);
+    const isAnswerRight = answer === ans;
+    console.log(isAnswerRight);
+  };
+
+  const handleNextBtnClick = () => {
+    setOptionClicked(false);
+    getQuestion(data);
+  };
+
   return (
-    <div className='app-container'>
+    <div className="app-container">
       <div>
         <h2>Country Quiz</h2>
-        <Card question={question} options={options} />
+        <Card
+          question={question}
+          options={options}
+          checkAnswer={checkAnswer}
+          handleNextBtnClick={handleNextBtnClick}
+          optionClicked={optionClicked}
+        />
       </div>
-    </div >
-  )
+    </div>
+  );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
-export default function Card({ question, options }) {
+export default function Card({ question, options, checkAnswer, optionClicked, handleNextBtnClick }) {
     return (
         <div className='quiz-card'>
             <h4>{question} is the capital of</h4>
 
             {options.sort().map((item, index) => {
                 const alph = ["A", "B", "C", "D"]
-                return <div key={index} className='options'>
+                return <div key={index} className='options' onClick={()=>checkAnswer(item)} >
                     <span>{alph[index]}</span><span>{item}</span>
                 </div>
             })}
+
+            {optionClicked && <button onClick={handleNextBtnClick}>Next</button>}
         </div>
     )
 }
