85b05281c4673812aad5df12aa048cc53bd82ca2
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-06-02 13:08:12 +0530

    ui init

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ import axios from 'axios';
 
 export default function App() {
   const [data, setData] = useState(null);
-  useEffect(()=>{
-    axios("https://restcountries.com/v3.1/all").then((res)=>setData(res.data))
-  },[])
+  useEffect(() => {
+    axios("https://restcountries.com/v3.1/all").then((res) => setData(res.data))
+  }, [])
   console.log(data);
   return (
     <div className='app-container'>
-      
+      <div>
+        <h2>Country Quiz</h2>
+        <div className='quiz-card'>
+          <h4>Delhi is the capital of</h4>
+          <div className='options'>
+            <span>A</span><span>India</span>
+          </div>
+          <div className='options'>
+            <span>b</span><span>India</span>
+          </div>
+          <div className='options'>
+            <span>c</span><span>India</span>
+          </div>
+          <div className='options'>
+            <span>d</span><span>India</span>
+          </div>
+      </div>
     </div>
+    </div >
   )
 }
